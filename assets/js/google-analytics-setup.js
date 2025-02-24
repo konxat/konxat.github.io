44d5d816cec9206ae49a6ffa8aa67afc72e555de
Konstantinos Chatzipapas <48755813+konxat@users.noreply.github.com>
2025-02-24 16:27:33 +0100

    Update google-analytics-setup.js

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -1,19 +1,12 @@
 window.dataLayer = window.dataLayer || [];
-function gtag() {
-  window.dataLayer.push(arguments);
-}
+window.addEventListener("load", function() {
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
 // gtag("js", new Date());
 // gtag("config", "{{ site.google_analytics }}");
 
-// Wait for gtag.js to load
-if (typeof gtag === "function") {
   gtag("js", new Date());
-  gtag("config", "{{ site.google_analytics }}");
-} else {
-  // Fallback: wait for gtag to be defined
-  window.addEventListener("load", function() {
-    gtag("js", new Date());
-    gtag("config", "{{ site.google_analytics }}");
-  });
-}
-gtag("event", "test_event", { "value": "debug" });
+  gtag("config", "{{ site.google_analytics }}"); 
+  gtag("event", "test_event", { "value": "debug" });
+});
