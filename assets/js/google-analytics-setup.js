13ac88182b7da055a9c3b9170b2f1a0fda1a1039
Konstantinos Chatzipapas <48755813+konxat@users.noreply.github.com>
2025-02-24 16:37:28 +0100

    Update and rename google-analytics-setup.js.liquid to google-analytics-setup.js

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -1,12 +1,13 @@
 window.dataLayer = window.dataLayer || [];
-window.addEventListener("load", function() {
-  function gtag() {
-    window.dataLayer.push(arguments);
-  }
+function gtag() {
+  window.dataLayer.push(arguments);
+}
 // gtag("js", new Date());
 // gtag("config", "{{ site.google_analytics }}");
 
+// Wait for page load to ensure gtag.js is ready
+window.addEventListener("load", function() {
   gtag("js", new Date());
-  gtag("config", "{{ site.google_analytics }}"); 
+  gtag("config", "G-EG5JPV9PZL");
   gtag("event", "test_event", { "value": "debug" });
 });
