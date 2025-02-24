d3fdb5446aef892d23eea1ba9080cd671bef8bba
Konstantinos Chatzipapas <48755813+konxat@users.noreply.github.com>
2025-02-24 16:58:58 +0100

    Update google-analytics-setup.js

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -8,6 +8,8 @@ function gtag() {
 // Wait for page load to ensure gtag.js is ready
 window.addEventListener("load", function() {
   gtag("js", new Date());
-  gtag("config", "G-EG5JPV9PZL");
+  gtag("config", "G-EG5JPV9PZL"); // In this file, the generic {{ site.google_analytics }} does not work and needs be replaced directly with the measurement ID. 
+                                  // However, this file is not used in the current implementation, and the setup is done dirrectly in the scripts.liquid file, 
+                                  // to maintain the genericness of the setup.
   gtag("event", "test_event", { "value": "debug" });
 });
