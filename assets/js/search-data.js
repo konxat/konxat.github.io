603ab7deaec4cf36b788b06ae4893b240d0aec35
konxat <konxat@users.noreply.github.com>
2025-11-12 10:47:34 +0000

    deploy: a7c3378ad372ba92fd0c50d2edf2d503894bf53a

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "members of Chatzipapas group",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
