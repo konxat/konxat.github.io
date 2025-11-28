12ace677f5ad0e21021c9454c409e0f04707e7bf
konxat <konxat@users.noreply.github.com>
2025-11-28 16:10:52 +0000

    deploy: bf2aea278460ccfdea0aea9520b2755b15ad112e

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -395,7 +395,7 @@ ninja.data = [{
           section: "News",},{id: "news-in-silico-assessment-of-cellular-damage-from-lu-177-ac-225-and-pb-212-therapeutic-radionuclides-sparkles-link-sparkles",
           title: 'In silico assessment of cellular damage from Lu-177, Ac-225, and Pb-212 therapeutic radionuclides....',
           description: "",
-          section: "News",},{id: "news-gate-10-monte-carlo-particle-transport-simulation-part-i-development-and-new-features-and-partii-architecture-and-innovations-sparkles-part-i-sparkles-part-ii-sparkles",
+          section: "News",},{id: "news-gate-10-monte-carlo-particle-transport-simulation-part-i-development-and-new-features-sparkles-part-i-sparkles-partii-architecture-and-innovations-sparkles-part-ii-sparkles",
           title: 'GATE 10 Monte Carlo particle transport simulation -Part I: development and new features...',
           description: "",
           section: "News",},{id: "projects-project-2",
